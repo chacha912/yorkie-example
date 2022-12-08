71699348ca1d865ac4c7ba0478d2b297091fdc81
chacha912 <dkwkdnwk912@gmail.com>
2022-12-09 00:58:52 +0900

    Add yorkie sample code

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+import yorkie from 'yorkie-js-sdk';
+
+async function main() {
+  const client = new yorkie.Client('https://api.yorkie.dev', {
+    apiKey: 'ce90b1juioqol8ent2u0',
+  });
+  await client.activate();
+
+  const doc = new yorkie.Document('my-first-document');
+  await client.attach(doc);
+
+  client.subscribe((event) => {
+    if (event.type === 'peers-changed') {
+      const peers = event.value[doc.getKey()];
+      document.getElementById('peersCount').innerHTML = Object.entries(peers).length;
+    }
+  });
+}
+main();
